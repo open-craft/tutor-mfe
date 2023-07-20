431a1af3fc3f1b19f1f5e2b4175344fa02828d88
Kshitij Sobti <kshitij@sobti.in>
2023-07-20 20:12:55 +0530

    Documented `iter_domains`. The dockerfile was also updated to optimize the process of setting up the environment, copying files, installing packages, and building MFEs for different domains and themes. The changes reduce repetition and improve the efficiency of the steps involved, by storing the base package dependencies in the `/openedx/base` directory, which can then be copied over and reused for each domain. This avoids redundant installations of the same dependencies for different domains.

--- tutormfe/plugin.py
+++ tutormfe/plugin.py
@@ -94,14 +94,29 @@ def iter_mfes() -> t.Iterable[tuple[str, MFE_ATTRS_TYPE]]:
     """
     yield from MFE_APPS.apply({}).items()
 
-def iter_domains(additional_domains, main_host, main_theme):
+def iter_domains(additional_domains: list[dict[str, str]], main_host:str, main_theme:str) -> t.Iterable[tuple[str, str]]:
     """
-    Yield:
+    Iterate over all domains that have an MFE enabled.
 
-        (name, theme)
+    It yields the name (domain) of each site and its corresponding theme.
+
+    Args:
+        additional_domains (list): A list of additional domain configurations.
+            Each domain configuration is a dictionary that should contain the keys 'domain',
+            'mfe_proxy', and 'mfe_theme'. The 'domain' key should contain the domain of the site,
+            the 'mfe_proxy' key should be set to the backend serving MFEs, but here it just
+            indicates whether MFEs are enabled for the site, and the 'mfe_theme' key should
+            contain the name of theme of the site.
+        main_host (str): The name of the main site.
+        main_theme (str): The theme of the main site.
+
+    Yield:
+        tuple[str, str]: A tuple containing the name (domain) and theme of each site.
     """
     yield main_host, main_theme
     for domain_config in additional_domains:
+        # If the domain has an mfe_proxy field it is serving MFEs. If it has mfe_theme set
+        # then we need to build a separate set of MFEs for it.
         if 'mfe_proxy' in domain_config and 'mfe_theme' in domain_config:
             yield domain_config['domain'], domain_config['mfe_theme']
 
