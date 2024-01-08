56039a8a3fe1fcc84b06b14777daf7978dc9aaf2
XnpioChV <xnpiochv@gmail.com>
2024-01-08 07:32:34 -0500

    chore: Update taxonomy sandbox branch

--- tutormfe/plugin.py
+++ tutormfe/plugin.py
@@ -31,7 +31,8 @@ config = {
 
 CORE_MFE_APPS: dict[str, MFE_ATTRS_TYPE] = {
     "course-authoring": {
-        "repository": "https://github.com/openedx/frontend-app-course-authoring.git",
+        "repository": "https://github.com/open-craft/frontend-app-course-authoring.git",
+        "version": "taxonomy-sandbox",
         "port": 2001,
     },
     # We activate this MFE to avoid using edx-platform views because of bugs on the sandbox.
